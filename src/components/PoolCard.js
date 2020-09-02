7e9779e56df6fbe003bd658974d6d28f8c5e7534
Akshay <araa@connect.ust.hk>
2020-09-02 23:54:35 +0800

    convert amounts to localString to display , in $ values

--- src/components/PoolCard.js
+++ src/components/PoolCard.js
@@ -5,16 +5,16 @@ const PoolCard = (props) => {
   const name = props.pool.poolToken.name;
   const apr = props.pool.apr.amount / 1000000;
   const balance = props.pool.balance.amount / 100000000000000000;
-  const amount = props.amount;
+  const amount = props.amount / 1;
   const yearly_earnings = (amount * apr) / 100;
   return (
     <div className="ui segment">
       <div>
         <div className="header">{name}</div>
-        <div>APY: {apr} % </div>
-        <div>Liquidity: ${balance} </div>
-        <div>You Deposit: ${amount} </div>
-        <div>Yearly Profit: ${yearly_earnings} </div>
+        <div>APY: {apr.toLocaleString()} % </div>
+        <div>Liquidity: ${balance.toLocaleString()} </div>
+        <div>You Deposit: ${amount.toLocaleString()} </div>
+        <div>Yearly Profit: ${yearly_earnings.toLocaleString()} </div>
       </div>
     </div>
   );
